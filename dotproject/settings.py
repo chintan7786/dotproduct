66048d91535e2be3c428b1a340d420a55e235d33
chintan7786 <davdachintan97@gmail.com>
2022-11-07 01:08:31 +0530

    v 2.0 final

--- dotproject/settings.py
+++ dotproject/settings.py
@@ -54,9 +54,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = "dotproject.urls"
 
